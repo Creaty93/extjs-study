b78028285392e58fc33ac54e72bbd943b7d2c6ac
stonb <stonb@221.166.184.23>
2022-01-31 18:52:01 +0900

    field test

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 
 Ext.onReady(function(){
-	Ext.create('Ext.container.Viewport',{
-		rederTo:Ext.getBody(),
+	Ext.create('Ext.panel.Panel',{
+		width:500,
+		height:500,
+		title:'form field',
+		renderTo:Ext.getBody(),
 		items:[{
-			xtype:'button',
-			text:'show modal',
-			handler:function(btn){
-				var win = Ext.create('Ext.window.Window',{
-		width:300,
-		height:300,
-		title:'Window Title',
-		autoShow:true,
-		modal:true,
-		items:[{
-			xtype:'button',
-			text:'window btn'
-		}]
-	});
-	
-	win.show();
-			}
+			xtype:'textfield',
+			allowBlank:false,
+			emptyText:'input text'
+		},{
+			xtype:'textfield',
+			inputType:'password',
+			allowBlank:false,
+			emptyText:'input password'
+		},{
+			xtype:'datefield',
+			format:'y-m-d'
+		},{
+			xtype:'numberfield',
+			minValue: -1,
+			maxValue:10
+		},{
+			xtype:'filefield',
+			buttonOnly:true
+		},{
+			xtype:'checkboxfield',
+			boxLabel:'아이디 기억'
+		},{
+			xtype:'radiofield',
+			name:'sex',
+			boxLabel:'남'
+		},{
+			xtype:'radiofield',
+			name:'sex',
+			boxLabel:'여'
+		},{
+			xtype:'slider',
+			width:100,
+			value:50
 		}]
 	})
 })
