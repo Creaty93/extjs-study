706ade586686b8e84115dbb49ad258c3d124b714
stonb <stonb@221.166.184.23>
2022-01-31 20:51:31 +0900

    tree 구조

--- app.js
+++ app.js
@@ -3,46 +3,26 @@ Ext.onReady(function(){
 	Ext.create('Ext.panel.Panel',{
 		width:500,
 		height:500,
-		title:'data store',
+		title:'tree panel',
 		renderTo:Ext.getBody(),
 		items:[{
-			xtype:'combo',
-			displayField:'key',
-			valueField:'value',
-			store:{
-				fields: ['key', 'value'],
-				proxy:{
-					type:'ajax',
-					url:'/data/combo.json',
-					reader:{
-						type:'json',
-						rootProperty:'data'
-					}
-				}
-			}
-		},{
-			xtype:'grid',
-			columns:[{
-				text:'컬럼1',
-				dataIndex:'column1'
-			},{
-				text:'컬럼2',
-				dataIndex:'column2'
-			},{
-				text:'컬럼3',
-				dataIndex:'column3'
-			}],
-			store:{
-				autoLoad:true,
-				fields: ['column1', 'column2', 'column3'],
-				proxy:{
-					type:'ajax',
-					url:'/data/grid.json',
-					reader:{
-						type:'json',
-						rootProperty:'data'
-					}
-				}
+			xtype:'treepanel',
+			root:{
+				text:'Servers',
+				expanded:false,
+				children:[{
+					text:'.settings',
+					expanded:true,
+					children:[{
+						text:'file1',
+						leaf:true
+					}]
+				},{
+					text:'.tomcat'
+				},{
+					text:'.project',
+					leaf:true
+				}]
 			}
 		}]
 	})
